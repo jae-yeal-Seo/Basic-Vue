2c6d0b5ae23176c76a2e045ae51d026b29ae5a72
woduf9173@naver.com <woduf9173@naver.com>
2021-07-16 19:51:41 +0900

    작성자, 댓글 입력

--- hello.js
+++ hello.js
@@ -1,7 +1,40 @@
-let app  = new Vue({
+// Vue.components('hello',{
+//     props:['name'],
+//     template:'<p>{{message}}</p>',
+//     data:function(){
+//         return{ message:'hello!,'+this.name }
+//     }
+// });
+
+// let app = new Vue({
+//     el:'#app',
+//     components:{
+//         'local-hello':{
+//             template:'<p>This is local component</p>'
+//         }
+//     }
+// });
+Vue.component('add',{
+    template:'<button @click="add">{{count}}</button>',
+    data(){
+        return {count:0}
+    },
+    methods:{
+        add(){
+            this.count+=1
+            this.$emit('event-add',1);
+        }
+    }
+})
+
+let app = new Vue({
     el:'#app',
     data:{
-        value:false,
-        values:[]
+        totalCount:0
+    },
+    methods:{
+        updateTotal(count){
+            this.totalCount+=count
+        }
     }
 });
